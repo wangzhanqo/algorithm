883b8254c395930378081cce0b70cf9f24def2cc
wangzhanqi <6466687@qq.com>
2025-08-18 16:03:42 +0800

    循环队列

--- src/main/java/org/example/queue/LoopQueue.java
+++ src/main/java/org/example/queue/LoopQueue.java
@@ -8,30 +8,72 @@ public class LoopQueue<E> implements Queue<E> {
 
     public LoopQueue(int capacity) {
         data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
     }
 
     @Override
     public void enqueue(E value) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+        }
+        data[tail] = value;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
 
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
     }
 
     @Override
     public E dequeue() {
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException();
+        }
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4) {
+            resize(getCapacity() / 2);
+        }
+        return ret;
     }
 
     @Override
     public E getFront() {
-        return null;
+        return data[front];
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return front == tail;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int i = 12;
+        int b = 7;
+        System.out.println(i % b);
     }
 }
